ff9c55772b76b6c3a1387d4aa526a6e5bf7128fb
Владислав Чернявский <cernavskijv877@gmail.com>
2024-09-02 23:30:04 +1000

    Код првведён к PEP8

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -5,11 +5,15 @@ from django.views.generic import TemplateView
 app_name = 'pages'
 
 urlpatterns = [
-     path('about/',
-         TemplateView.as_view(template_name='pages/about.html'),
-         name='about'),
+    path(
+        'about/',
+        TemplateView.as_view(template_name='pages/about.html'),
+        name='about'
+    ),
 
-     path('rules/',
-         TemplateView.as_view(template_name='pages/rules.html'),
-         name='rules'),
+    path(
+        'rules/',
+        TemplateView.as_view(template_name='pages/rules.html'),
+        name='rules'
+    ),
 ]
