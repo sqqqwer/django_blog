93743b0bde9c67e6c7b4221c1c9b256c7d9a27fe
Владислав Чернявский <cernavskijv877@gmail.com>
2024-09-05 15:58:36 +1000

    Убран неиспользуемый импорт

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.views.generic import (
     DetailView,
